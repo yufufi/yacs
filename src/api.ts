c6066a25d93bf290d133cb39fc0d067e0a3239a0
Y Furkan Fidan <dev@yufufi.com>
2017-03-30 22:48:06 +0100

    Conversion to typescript is mostly complete addings typings support though not verified

---
+++ src/api.ts
@@ -0,0 +1,84 @@
+// api.js
+// ======
+
+// storage.js
+// ==========
+class Branch {
+    constructor() {
+    }
+
+    static get Local() : string{
+        return "";
+    }
+}
+
+import * as Datastore from "nedb";
+
+class Data {
+
+    static _init: boolean;
+    static _db = Datastore;
+
+    static init() {
+        if (!Data._init) {
+
+            Data._currentMax = 0;
+
+            // Type 3: Persistent datastore with automatic loading 
+            Data._db = new Datastore({ filename: '.datafile', autoload: true });
+
+            Data._db.find({}).sort({index: -1}).limit(1).exec(function (err, doc) {
+                Data._currentMax = doc[0].index;
+            });
+            // You can issue commands right away 
+            
+            Data._init = true;
+        }
+    }
+
+    static get local() {
+        console.log("Requesting with: " + Data._currentMax);
+        var dataToBeReturned = undefined;
+        Data._db.find({index: Data._currentMax}).exec(function (err, doc) {
+                console.log("got the doc " + doc);
+                dataToBeReturned =  doc[0].data;
+        });
+        console.log("returning");
+        return dataToBeReturned;
+    }
+
+    static insert(input) {
+        Data._currentMax++;
+        Data._db.insert({ data: input, index: Data._currentMax});
+        console.log("Inserting " + input + " with: " + Data._currentMax);
+    }
+
+    static _currentMax : number;
+
+    toString() {
+        return ""; //this._data[0];
+    }
+}
+
+Data.init();
+
+module.exports = {
+    Data,
+    clip: function({ input = "" } = {}) {
+        Data.insert(input);
+        return {success: true};
+    },
+
+    paste: function() {
+        var d = Data.local;
+        console.log("d: " + d);
+        var z = {"paste": d};
+        console.log("z: " + z);
+        return z;
+    },
+
+    search: function({from = Branch.Local} = {}) {}
+};
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 var api = require('./api');
 
-app.get('/clip/:data', (req, res) => { res.json(api.clip(req.input)); });
+app.get('/clip/:data', (req, res) => { res.json(api.clip({input : req.input})); });
 app.get('/paste', (req, res) => { res.json(api.paste()); });
 
 
 app.param('data', function(request, response, next, data) {
     console.log(data);
-    request.input = new api.Data(data);
+    request.input = data;
     next();
 });
 
